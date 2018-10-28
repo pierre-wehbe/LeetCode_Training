939107e4f33e7d5b75b3ed3af0404eb7009bc2d9
pwehbe <pwehbe@atheerair.com>
2018-10-27 20:52:49 -0700

    Project Created

---
+++ LeetCode/LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by Pierre WEHBE on 10/27/18.
+//  Copyright © 2018 Atheer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
